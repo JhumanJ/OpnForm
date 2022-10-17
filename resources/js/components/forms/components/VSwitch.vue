40391dfdfed38a4ac27643606688ccf50218eb42
Chirag Chhatrala <chirag@notionforms.io>
2022-10-17 12:55:02 +0530

    toggle UI fixes

--- resources/js/components/forms/components/VSwitch.vue
+++ resources/js/components/forms/components/VSwitch.vue
@@ -1,7 +1,7 @@
 <template>
   <div @click="onClick">
     <div class="inline-flex items-center h-6 w-12 p-1 bg-gray-300 border rounded-full cursor-pointer focus:outline-none transition-all transform ease-in-out duration-100" :class="{'bg-nt-blue': internalValue}">
-      <div class="inline-block h-4 w-4 rounded-full bg-white shadow transition-all transform ease-in-out duration-150 rounded-2xl scale-100" :class="{'translate-x-6': internalValue}"></div>
+      <div class="inline-block h-4 w-4 rounded-full bg-white shadow transition-all transform ease-in-out duration-150 rounded-2xl scale-100" :class="{'translate-x-5.5': internalValue}" />
     </div>
   </div>
 </template>
@@ -34,7 +34,7 @@ export default {
   },
 
   methods: {
-    onClick(){
+    onClick () {
       this.$emit('input', !this.internalValue)
       this.internalValue = !this.internalValue
     }
