6a86c67b46ac5314254ccec86c100a4982611cf7
Chirag Chhatrala <chirag@notionforms.io>
2022-11-09 12:57:39 +0530

    form page divide into separate component & user route-view for tabs

---
+++ resources/js/pages/forms/show/submissions.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="table-page">
+    <form-submissions/>
+  </div>
+</template>
+
+<script>
+import FormSubmissions from '../../../components/open/forms/components/FormSubmissions'
+
+export default {
+  components: {FormSubmissions},
+  props: {
+    form: { type: Object, required: true }
+  },
+
+  metaInfo() {
+    return {title: 'Form Submissions'}
+  },
+
+  data: () => ({
+  }),
+
+  mounted() {},
+
+  computed: {},
+
+  methods: {
+    
+  }
+}
+</script>
