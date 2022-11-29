73cb016473579a0e1a7ee2750ae28255d4b1ecb9
Julien Nahum <jhumanj@MacBook-Pro-de-Julien.local>
2022-11-29 15:13:00 +0100

    Improve template pages

--- resources/js/pages/auth/components/QuickRegister.vue
+++ resources/js/pages/auth/components/QuickRegister.vue
@@ -3,10 +3,8 @@
     <!--  Login modal  -->
     <modal :show="showLoginModal" @close="showLoginModal=false" max-width="lg">
       <template #icon>
-        <svg class="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M12 8V16M8 12H16M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
         </svg>
       </template>
       <template #title>
@@ -21,10 +19,8 @@
     <!--  Register modal  -->
     <modal :show="showRegisterModal" @close="$emit('close')" max-width="lg">
       <template #icon>
-        <svg class="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M12 8V16M8 12H16M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
         </svg>
       </template>
       <template #title>
@@ -34,7 +30,7 @@
         <register-form :isQuick="true" @openLogin="openLogin" @afterQuickLogin="afterQuickLogin" />
       </div>
     </modal>
-    
+
   </div>
 </template>
 
