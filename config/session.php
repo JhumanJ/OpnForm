49bbb4112633de1c0c765ad72fb993ae80674df1
AAVision <75311052+AAVision@users.noreply.github.com>
2023-01-12 23:18:25 +0300

    changing session name for hiding laravel from web technology detection

--- config/session.php
+++ config/session.php
@@ -127,8 +127,7 @@ return [
     */
 
     'cookie' => env(
-        'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        'SESSION_COOKIE','OpnForm_session'
     ),
 
     /*
