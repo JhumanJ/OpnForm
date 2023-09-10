8ef193b6bb0044a4716677d55c868fbbeb6fa5f0
Sutirtha <sdas@republicfinance.com>
2023-09-10 16:59:59 +0530

    #170-ENH: Using VSelect component for country selection, added prop for dropdown styling

--- resources/js/components/forms/PhoneInput.vue
+++ resources/js/components/forms/PhoneInput.vue
@@ -12,35 +12,33 @@
         <slot name="help"><span class="field-help" v-html="help" /></slot>
       </small>
     </div>
-    <div v-on-clickaway="closeDropdown" class="relative">
-      <div :id="id ? id : name" :disabled="disabled" :name="name" class="flex">
-        <div class="dropdown" :style="inputStyle">
-          <div class="selected-option flex items-center justify-center space-x-2" @click="isOpen = !isOpen"
-            :class="[theme.default.input, { '!ring-red-500 !ring-2': hasValidation && form.errors.has(name), '!cursor-not-allowed !bg-gray-200': disabled }]"
-            :style="inputStyle">
-            <country-flag :country="selectedCountryCode.code" />
-            <span>{{ selectedCountryCode.dial_code }} </span>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 transform" :class="{ 'rotate-180': isOpen }"
-              fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-            </svg>
+    <div :id="id ? id : name" :disabled="disabled" :name="name" :style="inputStyle" class="flex items-center">
+      <v-select class="w-1/4" :data="countries" :value="selectedCountryCode" :inner-style="{ width: '474px' }"
+        :searchable="true" :search-keys="['name']" :option-key="'code'" :color="'#3B82F6'"
+        :placeholder="'Select a country'" :uppercase-labels="true" :theme="theme" @input="onCountryChange">
+        <template #option="props">
+          <div class="flex items-center space-x-2">
+            <country-flag :country="props.option.code" />
+            <span>{{ props.option.name }}</span>
+            <span>{{ props.option.code }}</span>
+            <span>{{ props.option.dial_code }}</span>
           </div>
-          <div class="options rounded-md bg-white dark:bg-notion-dark-light shadow-lg z-10" v-if="isOpen">
-            <div
-              class="option bg-white dark:bg-notion-dark-light z-10 flex items-center space-x-2 text-gray-900 cursor-default select-none relative py-2 pl-3 pr-9  cursor-pointer group hover:text-white hover:bg-nt-blue focus:outline-none focus:text-white focus:bg-nt-blue"
-              v-for="country in countries" :key="country.code" @click="onCountryChange(country)" :style="inputStyle">
-              <span><country-flag :country="country.code" /></span>
-              <span>{{ country.name }}</span>
-              <span>{{ country.code }}</span>
-              <span>{{ country.dial_code }}</span>
-            </div>
+        </template>
+        <template #selected="props">
+          <div class="flex items-center space-x-2 justify-start">
+            <country-flag :country="props.option.code" :style="{
+              'margin-top': '-0.7em',
+              'margin-left': '-0.9em',
+              'margin-right': '-0.6em',
+              'margin-bottom': '-0.7em'
+            }" />
+            <span>{{ props.option.dial_code }}</span>
           </div>
-        </div>
-        <input type="text" class="phone" v-model="inputVal"
-          :class="[theme.default.input, { '!ring-red-500 !ring-2': hasValidation && form.errors.has(name), '!cursor-not-allowed !bg-gray-200': disabled }]"
-          :placeholder="placeholder" :style="inputStyle" @input="onInput">
-      </div>
-
+        </template>
+      </v-select>
+      <input v-model="inputVal" type="text" class="inline-flex-grow ml-5"
+        :class="[theme.default.input, { '!ring-red-500 !ring-2': hasValidation && form.errors.has(name), '!cursor-not-allowed !bg-gray-200': disabled }]"
+        :placeholder="placeholder" :style="inputStyle" @input="onInput">
     </div>
   </div>
 </template>
@@ -50,12 +48,12 @@ import { directive as onClickaway } from 'vue-clickaway'
 import inputMixin from '~/mixins/forms/input.js'
 import countryCodes from '../../../data/country_codes.json'
 import CountryFlag from 'vue-country-flag'
-
+import VSelect from './components/VSelect.vue'
 
 export default {
   phone: 'PhoneInput',
   components: {
-    CountryFlag
+    CountryFlag, VSelect
   },
   directives: {
     onClickaway: onClickaway
@@ -78,7 +76,9 @@ export default {
       this.compVal = this.selectedCountryCode.dial_code + ' ' + newVal
     },
     selectedCountryCode(newVal, oldVal) {
-      this.compVal = this.compVal.replace(oldVal.dial_code, newVal.dial_code)
+      if (this.compVal) {
+        this.compVal = this.compVal.replace(oldVal.dial_code, newVal.dial_code)
+      }
     }
   },
   methods: {
@@ -98,34 +98,4 @@ export default {
 }
 </script>
 
-<style scoped>
-.dropdown {
-  position: relative;
-  width: 25%;
-}
-
-.options {
-  z-index: 10;
-  position: absolute;
-  width: 474px;
-  max-height: 200px;
-  overflow-y: auto;
-}
-
-.option {
-  padding: 10px;
-  cursor: pointer;
-}
-
-.selected-option {
-  padding: 10px;
-  cursor: pointer;
-}
-
-.phone {
-  flex-grow: 1;
-  margin-left: 10px;
-  padding: 10px;
-}
-
-</style>
+<style scoped></style>
