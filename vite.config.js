2ab3f798016b88b7a264f8d6b56c01255e6d8e40
Julien Nahum <julien@nahum.net>
2023-10-13 11:31:56 +0200

    Upload front-end sourcemap to sentry

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,42 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue2'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
+
+const plugins = [
+  laravel({
+    input: [
+      'resources/js/app.js'
+    ],
+    valetTls: 'opnform.test'
+  }),
+  vue({
+    template: {
+      transformAssetUrls: {
+        base: null,
+        includeAbsolute: false
+      }
+    }
+  })
+]
+
+if (process.env.SENTRY_AUTH_TOKEN) {
+  plugins.push(sentryVitePlugin({
+    org: 'opnform',
+    project: 'opnform',
+    authToken: process.env.SENTRY_AUTH_TOKEN
+  }))
+}
 
 export default defineConfig({
+  build: {
+    sourcemap: process.env.SENTRY_AUTH_TOKEN ? true : 'inline'
+  },
   esbuild: {
     minify: true,
     minifySyntax: true
   },
-  plugins: [
-    laravel({
-      input: [
-        'resources/js/app.js'
-      ],
-      valetTls: 'opnform.test'
-    }),
-    vue({
-      template: {
-        transformAssetUrls: {
-          base: null,
-          includeAbsolute: false
-        }
-      }
-    })
-  ],
+  plugins: plugins,
   optimizeDeps: {
     exclude: [
       'vt-notifications', 'vue-tailwind', 'vue-tailwind/dist/vue-tailwind.css'
