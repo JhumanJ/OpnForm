9c5610f3bb4780d3647a7e852098170f5e11ca1f
Forms Dev <chirag+new@notionforms.io>
2023-10-23 18:00:30 +0530

    Number input as Scale

--- resources/js/config/form-themes.js
+++ resources/js/config/form-themes.js
@@ -25,6 +25,12 @@ export const themes = {
       label: 'text-gray-700 dark:text-gray-300 font-semibold',
       input: 'relative w-full rounded-lg border-gray-300 flex-1 appearance-none border border-gray-300 dark:border-gray-600 w-full px-4 bg-white text-gray-700 placeholder-gray-400 dark:bg-notion-dark-light dark:text-gray-300 dark:placeholder-gray-600 shadow-sm text-base focus:outline-none focus:ring-2 focus:border-transparent',
       help: 'text-gray-400 dark:text-gray-500'
+    },
+    ScaleInput: {
+      label: 'text-gray-700 dark:text-gray-300 font-semibold',
+      button: 'cursor-pointer text-gray-700 inline-block rounded-lg border-gray-300 px-4 py-2 flex-grow dark:bg-notion-dark-light dark:text-gray-300 text-center',
+      unselectedButton: 'bg-white hover:bg-gray-50 border',
+      help: 'text-gray-400 dark:text-gray-500'
     }
   },
   simple: {
@@ -50,6 +56,12 @@ export const themes = {
       label: 'text-gray-700 dark:text-gray-300 font-semibold',
       input: 'border-transparent flex-1 appearance-none border border-gray-300 dark:border-gray-600 w-full bg-white text-gray-700 dark:bg-notion-dark-light dark:text-gray-300 dark:placeholder-gray-500 placeholder-gray-400 text-base focus:outline-none focus:ring-1 focus:ring-opacity-100 focus:border-transparent focus:ring-2',
       help: 'text-gray-400 dark:text-gray-500'
+    },
+    ScaleInput: {
+      label: 'text-gray-700 dark:text-gray-300 font-semibold',
+      button: 'flex-1 appearance-none border-gray-300 dark:border-gray-600 w-full py-2 px-2 bg-gray-50 text-gray-700 dark:bg-notion-dark-light dark:text-gray-300 text-center',
+      unselectedButton: 'bg-white hover:bg-gray-50 border -mx-4',
+      help: 'text-gray-400 dark:text-gray-500'
     }
   },
   notion: {
@@ -74,6 +86,12 @@ export const themes = {
     RichTextAreaInput: {
       label: 'text-gray-900 dark:text-gray-100 mb-2 block mt-4',
       input: 'rounded border-transparent flex-1 appearance-none shadow-inner-notion border border-gray-300 dark:border-gray-600 w-full text-gray-900 bg-notion-input-background dark:bg-notion-dark-light shadow-inner dark:placeholder-gray-500 placeholder-gray-400 text-base focus:outline-none focus:ring-0 focus:ring-opacity-100 focus:border-transparent focus:ring-0 focus:shadow-focus-notion',
+      help: 'text-notion-input-help dark:text-gray-500'
+    },
+    ScaleInput: {
+      label: 'text-gray-900 dark:text-gray-100 mb-2 block mt-4',
+      button: 'rounded border-transparent flex-1 appearance-none shadow-inner-notion w-full py-2 px-2 bg-notion-input-background dark:bg-notion-dark-light text-gray-900 dark:text-gray-100 text-center',
+      unselectedButton: 'bg-notion-input-background dark:bg-notion-dark-light hover:bg-gray-50 border',
       help: 'text-notion-input-help dark:text-gray-500'
     }
   }
