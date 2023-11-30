164b8c4ddd85aaabd4793b3ae979463146621155
Julien Nahum <julien@nahum.net>
2023-11-30 11:31:56 +0100

    Fix the custom domain default redirect

--- resources/js/middleware/custom-domains.js
+++ resources/js/middleware/custom-domains.js
@@ -1,11 +1,11 @@
 export default async (to, from, next) => {
-  if (!window.config.custom_domains) {
+  if (!window.config.custom_domains_enabled) {
     next()
   }
   const isCustomDomain = getDomain(window.location.href) !== getDomain(window.config.app_url)
   if (isCustomDomain && !['forms.show_public'].includes(to.name)) {
     // If route isn't a public form, redirect
-    window.location.href = 'https://opnform.com/'
+    window.location.href = 'https://opnform.com/?utm_source=failed_custom_domain_redirect'
   } else {
     next()
   }
