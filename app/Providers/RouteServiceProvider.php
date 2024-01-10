eea2ac59efc6cb9b1fb96bb09a2a8b46183490c7
Forms Dev <chirag+new@notionforms.io>
2024-01-10 16:21:38 +0530

    PR changes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -47,10 +47,6 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
-
-            Route::middleware('spa')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/spa.php'));
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 /*
 |--------------------------------------------------------------------------
