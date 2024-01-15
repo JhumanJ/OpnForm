e41929e4c8914c17bc8d00d36345d0063134c793
Julien Nahum <julien@nahum.net>
2024-01-15 16:17:10 +0100

    Debug getDomain in prod

--- client/lib/utils.js
+++ client/lib/utils.js
@@ -60,7 +60,12 @@ export const getHost = function () {
  */
 export const getDomain = function (url) {
   if (url.includes('localhost')) return 'localhost'
-  return (new URL(url)).hostname
+  try {
+    return (new URL(url)).hostname
+  } catch (e) {
+    console.error('Error extracting domai from: ',url,e)
+    return null
+  }
 }
 
 /**
