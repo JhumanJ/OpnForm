469a3ebf4a79326e12c6853934b6d4cb3e3fe3df
Julien Nahum <julien@nahum.net>
2024-01-16 19:34:03 +0100

    Fix footer

--- client/components/pages/OpenFormFooter.vue
+++ client/components/pages/OpenFormFooter.vue
@@ -7,10 +7,10 @@
         </p>
       </div>
       <div class="flex justify-center mt-5 md:mt-0">
-        <router-link :to="{ name: user ? 'home' : 'welcome' }" class="flex-shrink-0 font-semibold flex items-center">
+        <router-link :to="{ name: user ? 'home' : 'index' }" class="flex-shrink-0 font-semibold flex items-center">
           <img src="/img/logo.svg" alt="notion tools logo" class="w-10 h-10">
           <span class="ml-2 text-xl text-black dark:text-white">
-            {{ appName }}
+            OpnForm
           </span>
         </router-link>
       </div>
