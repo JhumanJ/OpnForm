a34ae4a65fb17fab04e9d0503daca673f95379ce
Julien Nahum <julien@nahum.net>
2024-01-24 17:59:23 +0100

    Fix sibebar disapear animation

--- client/components/open/editors/EditorRightSidebar.vue
+++ client/components/open/editors/EditorRightSidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <transition @leave="(el,done) => sidebarMotion.leave(done)">
+  <transition @leave="(el,done) => sidebarMotion?.leave(done)">
     <div v-if="show" ref="sidebar"
          class="absolute shadow-lg shadow-gray-800/30 top-0 h-[calc(100vh-53px)] right-0 lg:shadow-none lg:relative bg-white w-full md:w-1/2 lg:w-2/5 border-l overflow-y-scroll md:max-w-[20rem] flex-shrink-0 z-50"
     >
