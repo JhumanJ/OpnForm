381824183c2f63794a9380f6c3daa3fba27d5f76
Julien Nahum <julien@nahum.net>
2024-01-29 10:25:00 +0100

    Fix form AI creation bug + use gpt4 turbo with Json mode

--- client/composables/useCrisp.js
+++ client/composables/useCrisp.js
@@ -64,7 +64,7 @@ export const useCrisp = () => {
 
   function pushEvent(event, data = {}) {
     if (!crisp) return
-    crisp.pushEvent(event, data)
+    crisp.session.pushEvent(event, data)
   }
 
   function setSegments(segments, overwrite = false) {
