504fef759412475b8021369105d8bb1ced2a5c8d
Julien Nahum <julien@nahum.net>
2024-02-02 16:07:11 +0100

    Fixed the submission loading issue

--- client/stores/records.js
+++ client/stores/records.js
@@ -9,11 +9,11 @@ export const useRecordsStore = defineStore('records', ()=>{
   const contentStore = useContentStore()
 
   const loadRecord = (request)=> {
-    this.contentStore.resetState()
-    this.contentStore.startLoading()
+    contentStore.resetState()
+    contentStore.startLoading()
     return request.then((data) => {
-      this.contentStore.save(data)
-      this.contentStore.stopLoading()
+      contentStore.save(data)
+      contentStore.stopLoading()
     })
   }
   return {...contentStore, loadRecord}
