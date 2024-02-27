7c90ed58bd95de73d425fe82271a9b54ac8fcb52
Julien Nahum <julien@nahum.net>
2024-02-27 09:54:06 +0100

    Change form created email

--- resources/views/mail/form/created.blade.php
+++ resources/views/mail/form/created.blade.php
@@ -10,17 +10,16 @@ If you want to share your form, here is the link:
 {{$form->share_url}}
 @endcomponent
 
-If you want to **embed your form in Notion**, simply type "/embed", confirm, and paste the URL above.
-If you want to add the form to your website, copy and paste the following code:
+If you want to embed your form to your website, copy and paste the following code:
 @component('mail::panel')
- {{ '<iframe style="border:none;width:100%;" height="620px" src="'.$form->share_url.'"></iframe>' }}
+{{ '<iframe style="border:none;width:100%;" height="620px" src="'.$form->share_url.'"></iframe>' }}
 @endcomponent
 
-Finally, we created a **Facebook group** with all the other users to share all the exciting news and tutorials about NotionForms.
+Finally, we created a **Discord channel** with all the other users to share our news and guides about OpnForm.
 I would love to see you there, here is the link:
 
-@component('mail::button', ['url' => config('links.facebook_group')])
-Join Facebook Group
+@component('mail::button', ['url' => config('links.discord')])
+Discord Channel
 @endcomponent
 
 @endcomponent
