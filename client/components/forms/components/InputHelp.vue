1248c15f5b80a272af1a32ccfe5f468185d685f8
Frank <csskfaves@gmail.com>
2024-03-12 09:52:54 +0100

    fix null message

--- client/components/forms/components/InputHelp.vue
+++ client/components/forms/components/InputHelp.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex mb-1 input-help">
     <small :class="theme.default.help" class="grow flex">
-      <slot name="help"><span class="field-help" v-html="helpValue" /></slot>
+      <slot name="help"><span v-if="help" class="field-help" v-html="help" /></slot>
     </small>
     <slot name="after-help">
       <small class="flex-grow"  />
@@ -16,12 +16,6 @@ export default {
   props: {
     theme: { type: Object, required: true },
     help: { type: String, required: false }
-  },
-
-  computed: {
-    helpValue(){
-      return (this.help === null || this.help === undefined) ? '' : this.help
-    }
   }
 }
 </script>
