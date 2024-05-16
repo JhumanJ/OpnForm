795fcfc97384e227d95d90a87af42b345276a4c7
Chirag Chhatrala <60499540+chiragchhatrala@users.noreply.github.com>
2024-05-16 18:20:40 +0530

    Select Design Changes (#409)
    * Select Design Changes
    
    * update theme file for SelectInput
    
    ---------
    
    Co-authored-by: Julien Nahum <julien@nahum.net>

--- client/components/forms/components/VSelect.vue
+++ client/components/forms/components/VSelect.vue
@@ -4,24 +4,18 @@
     class="v-select relative"
     :class="[{ 'w-0': multiple, 'min-w-full': multiple }]"
   >
-    <span class="inline-block w-full rounded-md">
+    <div
+      class="inline-block w-full flex overflow-hidden"
+      :style="inputStyle"
+      :class="[theme.SelectInput.input, { '!ring-red-500 !ring-2 !border-transparent': hasError, '!cursor-not-allowed !bg-gray-200': disabled }, inputClass]"
+    >
       <button
         type="button"
         aria-haspopup="listbox"
         aria-expanded="true"
         aria-labelledby="listbox-label"
-        class="cursor-pointer"
-        :style="inputStyle"
-        :class="[
-          theme.SelectInput.input,
-          {
-            'py-2': !multiple || loading,
-            'py-1': multiple,
-            '!ring-red-500 !ring-2 !border-transparent': hasError,
-            '!cursor-not-allowed !bg-gray-200': disabled,
-          },
-          inputClass,
-        ]"
+        class="cursor-pointer w-full flex-grow relative"
+        :class="[{'py-2': !multiple || loading, 'py-1': multiple},theme.default.inputSpacing.horizontal]"
         @click="toggleDropdown"
       >
         <div :class="{ 'h-6': !multiple, 'min-h-8': multiple && !loading }">
@@ -43,6 +37,7 @@
               <slot
                 name="selected"
                 :option="modelValue"
+                :toggle="select"
               />
             </div>
             <div
@@ -60,51 +55,55 @@
             </div>
           </transition>
         </div>
-        <span
-          class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
-        >
-          <svg
-            class="h-5 w-5 text-gray-400"
-            viewBox="0 0 20 20"
-            fill="none"
-            stroke="currentColor"
-          >
-            <path
-              d="M7 7l3-3 3 3m0 6l-3 3-3-3"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
+        <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+          <Icon
+            name="heroicons:chevron-up-down-16-solid"
+            class="h-5 w-5 text-gray-500"
+          />
         </span>
       </button>
-    </span>
+      <button
+        v-if="clearable && !isEmpty"
+        class="hover:bg-gray-50 dark:hover:bg-gray-900 border-l px-2"
+        :class="[theme.default.inputSpacing.vertical]"
+        @click.prevent="clear()"
+      >
+        <Icon
+          name="heroicons:x-mark-20-solid"
+          class="w-5 h-5 text-gray-500"
+          width="2em"
+          dynamic
+        />
+      </button>
+    </div>
     <collapsible
       v-model="isOpen"
-      class="absolute mt-1 rounded-md bg-white dark:bg-notion-dark-light shadow-xl z-10"
-      :class="dropdownClass"
+      class="absolute mt-1 bg-white overflow-auto dark:bg-notion-dark-light shadow-xl z-10"
+      :class="[dropdownClass,theme.SelectInput.dropdown]"
       @click-away="onClickAway"
     >
       <ul
         tabindex="-1"
         role="listbox"
-        class="rounded-md text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5 relative"
-        :class="{
-          'max-h-42 py-1': !isSearchable,
-          'max-h-48 pb-1': isSearchable,
-        }"
+        class="text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5 relative"
+        :class="{ 'max-h-42': !isSearchable, 'max-h-48': isSearchable }"
       >
         <div
           v-if="isSearchable"
-          class="px-2 pt-2 sticky top-0 bg-white dark-bg-notion-dark-light z-10"
+          class="sticky top-0 z-10 flex border-b border-gray-300"
         >
-          <text-input
+          <input
             v-model="searchTerm"
-            name="search"
-            :color="color"
-            :theme="theme"
-            placeholder="Search..."
-          />
+            type="text"
+            class="flex-grow pl-3 pr-7 py-3 w-full focus:outline-none dark:text-white"
+            placeholder="Search"
+          >
+          <div class="flex absolute right-0 inset-y-0 items-center px-2 justify-center pointer-events-none">
+            <Icon
+              name="heroicons:magnifying-glass-solid"
+              class="h-5 w-5 text-gray-500 dark:text-gray-400"
+            />
+          </div>
         </div>
         <div
           v-if="loading"
@@ -112,14 +111,17 @@
         >
           <Loader class="h-6 w-6 text-nt-blue mx-auto" />
         </div>
-        <template v-if="filteredOptions.length > 0">
+        <div
+          v-if="filteredOptions.length > 0"
+          class="p-1"
+        >
           <li
             v-for="item in filteredOptions"
             :key="item[optionKey]"
             role="option"
             :style="optionStyle"
-            :class="{ 'px-3 pr-9': multiple, 'px-3': !multiple }"
-            class="text-gray-900 cursor-default select-none relative py-2 cursor-pointer group hover:text-white hover:bg-form-color focus:outline-none focus-text-white focus-nt-blue"
+            :class="[{ 'px-3 pr-9': multiple, 'px-3': !multiple },dropdownClass,theme.SelectInput.option]"
+            class="text-gray-900 cursor-default select-none relative py-2 cursor-pointer group hover:bg-gray-100 dark:hover:bg-gray-900 rounded focus:outline-none"
             @click="select(item)"
           >
             <slot
@@ -128,131 +130,128 @@
               :selected="isSelected(item)"
             />
           </li>
-        </template>
+        </div>
         <p
           v-else-if="!loading && !(allowCreation && searchTerm)"
           class="w-full text-gray-500 text-center py-2"
         >
-          {{
-            allowCreation
-              ? "Type something to add an option"
-              : "No option available"
-          }}.
+          {{ (allowCreation ? 'Type something to add an option' : 'No option available') }}.
         </p>
-        <li
+        <div
           v-if="allowCreation && searchTerm"
-          role="option"
-          :style="optionStyle"
-          :class="{ 'px-3 pr-9': multiple, 'px-3': !multiple }"
-          class="text-gray-900 cursor-default select-none relative py-2 cursor-pointer group hover:text-white dark:text-white hover:bg-form-color focus:outline-none focus-text-white focus-nt-blue"
-          @click="createOption(searchTerm)"
+          class="border-t border-gray-300 p-1"
         >
-          Create
-          <b class="px-1 bg-gray-300 rounded group-hover-text-black">{{
-            searchTerm
-          }}</b>
-        </li>
+          <li
+            role="option"
+            :style="optionStyle"
+            :class="[{ 'px-3 pr-9': multiple, 'px-3': !multiple },dropdownClass,theme.SelectInput.option]"
+            class="text-gray-900 cursor-default select-none relative py-2 cursor-pointer group hover:bg-gray-100 dark:hover:bg-gray-900 rounded focus:outline-none"
+            @click="createOption(searchTerm)"
+          >
+            Create <span class="px-2 bg-gray-100 border border-gray-300 rounded group-hover-text-black">{{ searchTerm
+            }}</span>
+          </li>
+        </div>
       </ul>
     </collapsible>
   </div>
 </template>
 
 <script>
-import Collapsible from "~/components/global/transitions/Collapsible.vue"
-import { themes } from "~/lib/forms/form-themes.js"
-import TextInput from "../TextInput.vue"
-import debounce from "lodash/debounce"
-import Fuse from "fuse.js"
+import Collapsible from '~/components/global/transitions/Collapsible.vue'
+import { themes } from '../../../lib/forms/form-themes.js'
+import debounce from 'debounce'
+import Fuse from 'fuse.js'
 
 export default {
-  name: "VSelect",
-  components: { Collapsible, TextInput },
+  name: 'VSelect',
+  components: { Collapsible },
   directives: {},
   props: {
     data: Array,
     modelValue: { default: null, type: [String, Number, Array, Object] },
     inputClass: { type: String, default: null },
-    dropdownClass: { type: String, default: "w-full" },
+    dropdownClass: { type: String, default: 'w-full' },
     loading: { type: Boolean, default: false },
     required: { type: Boolean, default: false },
     multiple: { type: Boolean, default: false },
     searchable: { type: Boolean, default: false },
+    clearable: { type: Boolean, default: false },
     hasError: { type: Boolean, default: false },
     remote: { type: Function, default: null },
-    searchKeys: { type: Array, default: () => ["name"] },
-    optionKey: { type: String, default: "id" },
+    searchKeys: { type: Array, default: () => ['name'] },
+    optionKey: { type: String, default: 'id' },
     emitKey: { type: String, default: null },
-    color: { type: String, default: "#3B82F6" },
+    color: { type: String, default: '#3B82F6' },
     placeholder: { type: String, default: null },
     uppercaseLabels: { type: Boolean, default: true },
     theme: { type: Object, default: () => themes.default },
     allowCreation: { type: Boolean, default: false },
-    disabled: { type: Boolean, default: false },
+    disabled: { type: Boolean, default: false }
   },
-  emits: ["update:modelValue", "update-options"],
-  data() {
+  emits: ['update:modelValue', 'update-options'],
+  data () {
     return {
       isOpen: false,
-      searchTerm: "",
-      defaultValue: this.modelValue ?? null,
+      searchTerm: '',
+      defaultValue: this.modelValue ?? null
     }
   },
   computed: {
-    optionStyle() {
+    optionStyle () {
       return {
-        "--bg-form-color": this.color,
+        '--bg-form-color': this.color
       }
     },
-    inputStyle() {
+    inputStyle () {
       return {
-        "--tw-ring-color": this.color,
+        '--tw-ring-color': this.color
       }
     },
-    debouncedRemote() {
+    debouncedRemote () {
       if (this.remote) {
         return debounce(this.remote, 300)
       }
       return null
     },
-    filteredOptions() {
+    filteredOptions () {
       if (!this.data) return []
-      if (!this.searchable || this.remote || this.searchTerm === "") {
+      if (!this.searchable || this.remote || this.searchTerm === '') {
         return this.data
       }
 
       // Fuse search
       const fuzeOptions = {
-        keys: this.searchKeys,
+        keys: this.searchKeys
       }
       const fuse = new Fuse(this.data, fuzeOptions)
       return fuse.search(this.searchTerm).map((res) => {
         return res.item
       })
     },
-    isSearchable() {
+    isSearchable () {
       return this.searchable || this.remote !== null || this.allowCreation
     },
+    isEmpty () {
+      return this.multiple ? !this.modelValue || this.modelValue.length === 0 : !this.modelValue
+    }
   },
   watch: {
-    searchTerm(val) {
+    searchTerm (val) {
       if (!this.debouncedRemote) return
-      if (
-        (this.remote && val) ||
-        (val === "" && !this.modelValue) ||
-        (val === "" && this.isOpen)
-      ) {
+      if ((this.remote && val) || (val === '' && !this.modelValue) || (val === '' && this.isOpen)) {
         return this.debouncedRemote(val)
       }
-    },
+    }
   },
   methods: {
-    onClickAway(event) {
+    onClickAway (event) {
       // Check that event target isn't children of dropdown
       if (this.$refs.select && !this.$refs.select.contains(event.target)) {
         this.isOpen = false
       }
     },
-    isSelected(value) {
+    isSelected (value) {
       if (!this.modelValue) return false
 
       if (this.emitKey && value[this.emitKey]) {
@@ -264,17 +263,17 @@ export default {
       }
       return this.modelValue === value
     },
-    toggleDropdown() {
+    toggleDropdown () {
       if (this.disabled) {
         this.isOpen = false
       } else {
         this.isOpen = !this.isOpen
       }
       if (!this.isOpen) {
-        this.searchTerm = ""
+        this.searchTerm = ''
       }
     },
-    select(value) {
+    select (value) {
       if (!this.multiple) {
         // Close after select
         this.toggleDropdown()
@@ -285,48 +284,43 @@ export default {
       }
 
       if (this.multiple) {
-        const emitValue = Array.isArray(this.modelValue)
-          ? [...this.modelValue]
-          : []
+        const emitValue = Array.isArray(this.modelValue) ? [...this.modelValue] : []
 
         if (this.isSelected(value)) {
-          this.$emit(
-            "update:modelValue",
-            emitValue.filter((item) => {
-              if (this.emitKey) {
-                return item !== value
-              }
-              return (
-                item[this.optionKey] !== value &&
-                item[this.optionKey] !== value[this.optionKey]
-              )
-            }),
-          )
+          this.$emit('update:modelValue', emitValue.filter((item) => {
+            if (this.emitKey) {
+              return item !== value
+            }
+            return item[this.optionKey] !== value && item[this.optionKey] !== value[this.optionKey]
+          }))
           return
         }
 
         emitValue.push(value)
-        this.$emit("update:modelValue", emitValue)
+        this.$emit('update:modelValue', emitValue)
       } else {
         if (this.modelValue === value) {
-          this.$emit("update:modelValue", this.defaultValue ?? null)
+          this.$emit('update:modelValue', this.defaultValue ?? null)
         } else {
-          this.$emit("update:modelValue", value)
+          this.$emit('update:modelValue', value)
         }
       }
     },
-    createOption(newOption) {
+    clear () {
+      this.$emit('update:modelValue', this.multiple ? [] : null)
+    },
+    createOption (newOption) {
       if (newOption) {
         const newItem = {
           name: newOption,
           value: newOption,
-          id: newOption,
+          id: newOption
         }
-        this.$emit("update-options", newItem)
+        this.$emit('update-options', newItem)
         this.select(newItem)
-        this.searchTerm = ""
+        this.searchTerm = ''
       }
-    },
-  },
+    }
+  }
 }
 </script>
