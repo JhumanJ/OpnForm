c5720ce2f4102934bd7c4d7fbb97d6d6de87f591
Boris Lepikhin <lepikhinb@gmail.com>
2024-05-17 20:41:22 -0700

    fix `helpers.php`

--- app/helpers.php
+++ app/helpers.php
@@ -1,11 +1,13 @@
 <?php
 
-function front_url($path = '')
-{
-    $baseUrl = config('app.front_url');
-    if (! $baseUrl) {
-        return $path;
-    }
+if(!function_exists('front_url')) {
+    function front_url($path = '')
+    {
+        $baseUrl = config('app.front_url');
+        if (! $baseUrl) {
+            return $path;
+        }
 
-    return rtrim($baseUrl, '/').'/'.ltrim($path, '/');
+        return rtrim($baseUrl, '/').'/'.ltrim($path, '/');
+    }
 }
