876092500797a302e762d67d97126d8edf5bc977
Favour Olayinka <csskfaves@gmail.com>
2024-05-22 13:36:42 +0100

    fix field width offset (#417)
    * fix store content bug
    
    * fix: form field indexing bug on working form store
    
    * fix column width on  form fied
    
    * fix field width offset
    
    * UI polishing
    
    * Fix cursor grab
    
    ---------
    
    Co-authored-by: Julien Nahum <julien@nahum.net>

--- client/components/open/forms/OpenFormField.vue
+++ client/components/open/forms/OpenFormField.vue
@@ -2,11 +2,13 @@
   <div
     v-if="!isFieldHidden"
     :id="'block-' + field.id"
+    class=""
     :class="[
       getFieldWidthClasses(field),
       {
-        'group/nffield hover:bg-gray-100/50 relative hover:z-10 mx-[-15px] px-[15px] transition-colors hover:border-gray-200 dark:hover:bg-gray-900 border-dashed border border-transparent box-border dark:hover:border-blue-900 rounded-md':adminPreview,
-        'bg-blue-50 hover:!bg-blue-50 dark:bg-gray-800 rounded-md': beingEdited
+        'group/nffield hover:bg-gray-100/50 relative hover:z-10 px-2 transition-colors hover:border-gray-200 dark:hover:bg-gray-900 border-dashed border border-transparent box-border dark:hover:border-blue-900 rounded-md':adminPreview,
+        'bg-blue-50 hover:!bg-blue-50 dark:bg-gray-800 rounded-md': beingEdited,
+        'mx-[15px]' : !adminPreview
       }]"
   >
     <div
@@ -250,15 +252,15 @@ export default {
     getFieldWidthClasses(field) {
       if (!field.width || field.width === 'full') return 'col-span-full'
       else if (field.width === '1/2') {
-        return 'w-full sm:col-span-6 col-span-full'
+        return 'sm:col-span-6 col-span-full'
       } else if (field.width === '1/3') {
-        return 'w-full sm:col-span-4 col-span-full'
+        return 'sm:col-span-4 col-span-full'
       } else if (field.width === '2/3') {
-        return 'w-full sm:col-span-8 col-span-full'
+        return 'sm:col-span-8 col-span-full'
       } else if (field.width === '1/4') {
-        return 'w-full sm:col-span-3 col-span-full'
+        return 'sm:col-span-3 col-span-full'
       } else if (field.width === '3/4') {
-        return 'w-full sm:col-span-9 col-span-full'
+        return 'sm:col-span-9 col-span-full'
       }
     },
     getFieldAlignClasses(field) {
