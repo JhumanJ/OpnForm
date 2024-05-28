0b557d5b37617bfb925034b53310de987594836c
Shift <shift@laravelshift.com>
2024-05-28 09:51:46 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command('forms:database-cleanup')->hourly();
+Schedule::command('forms:integration-events-cleanup')->daily();
