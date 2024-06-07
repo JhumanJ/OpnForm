ac174cb6bd1061a1700763d50adfe6a15caeb9e1
Frank <csskfaves@gmail.com>
2024-06-07 16:14:07 +0100

    linting

--- database/migrations/2023_03_06_033440_change_max_submissions_reached_text_to_text.php
+++ database/migrations/2023_03_06_033440_change_max_submissions_reached_text_to_text.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-return new class() extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
