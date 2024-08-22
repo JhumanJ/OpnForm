70b8b9134528983d8bb595fad8a687714add7a36
Chirag Chhatrala <chirag.chhatrala@gmail.com>
2024-08-22 22:50:35 +0530

    Autofocus first field as settings option

---
+++ database/migrations/2024_08_22_141621_add_auto_focus_to_forms.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('forms', function (Blueprint $table) {
+            $table->boolean('auto_focus')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('forms', function (Blueprint $table) {
+            $table->dropColumn('auto_focus');
+        });
+    }
+};
