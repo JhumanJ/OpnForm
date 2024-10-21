2fdf2a439b0ba988965fe9cca7328ba7c31db911
Julien Nahum <julien@nahum.net>
2024-10-21 19:51:35 +0200

    Bump versions

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
         '@pinia/nuxt',
         '@vueuse/nuxt',
         '@vueuse/motion/nuxt',
-        'nuxt-simple-sitemap',
+        '@nuxtjs/sitemap',
         '@nuxt/ui',
         'nuxt-utm',
         ...process.env.NUXT_PUBLIC_GTM_CODE ? ['@zadigetvoltaire/nuxt-gtm'] : [],
