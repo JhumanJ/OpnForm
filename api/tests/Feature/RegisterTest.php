45c2b5f6bd5be2abee46cea775b9bbef9052a5ed
Chirag Chhatrala <chirag.chhatrala@gmail.com>
2024-12-17 12:33:05 +0530

    fix pint

--- api/tests/Feature/RegisterTest.php
+++ api/tests/Feature/RegisterTest.php
@@ -77,7 +77,7 @@ it('cannot register with disposable email', function () {
 });
 
 it('requires hcaptcha token in production', function () {
-    app()->detectEnvironment(fn() => 'production');
+    app()->detectEnvironment(fn () => 'production');
 
     Http::fake([
         ValidHCaptcha::H_CAPTCHA_VERIFY_URL => Http::response(['success' => true])
