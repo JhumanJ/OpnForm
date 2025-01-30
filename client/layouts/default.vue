43365547c79d20f41fd2d73f5e142af55a85f11b
Anton <antonmerkotun@gmail.com>
2025-01-30 13:43:33 +0100

    fix sidebar

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="main-layout min-h-screen flex flex-col h-full">
-    <!--    <Navbar />-->
-    <Header />
+    <Header v-if="isFormsPage" />
+    <Navbar v-else />
 
-    <div class="flex w-full mx-auto max-w-[1440px] h-full">
-      <sidebar />
+    <div
+      class="w-full mx-auto max-w-[1440px] h-full"
+      :class="{ flex: isFormsPage }"
+    >
+      <sidebar v-if="isFormsPage" />
 
       <slot class="flex-grow" />
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 import Sidebar from "~/layouts/sidebar.vue"
 import Header from "~/components/global/Header.vue"
+import Navbar from "~/components/global/Navbar.vue"
+import { useRoute } from "vue-router"
+import { computed } from "vue"
+
+const route = useRoute()
+
+const isFormsPage = computed(() => route.path.includes("forms"))
 </script>
