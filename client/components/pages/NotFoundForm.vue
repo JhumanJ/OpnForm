591da872979b477606809383fe0e5568f19ebbdb
Julien Nahum <julien@nahum.net>
2025-03-20 16:03:08 +0100

    Refactor Navbar and NotFoundForm Components for Improved User Experience
    - Simplify the Navbar component by removing unnecessary conditional rendering for the authentication display, enhancing clarity in the layout.
    - Update the NotFoundForm component to utilize a dynamic actions array for rendering navigation options, improving maintainability and flexibility.
    - Adjust text styles for better readability and consistency across the NotFoundForm component.
    
    These changes aim to streamline the user interface and enhance the overall user experience when navigating forms and handling not found scenarios.

--- client/components/pages/NotFoundForm.vue
+++ client/components/pages/NotFoundForm.vue
@@ -7,11 +7,11 @@
           alt="404 Error"
           class="mx-auto w-1/3"
         >
-        <h1 class="text-dark text-2xl md:text-4xl leading-none font-semibold text-center pt-4 md:pt-0 mx-auto md:max-w-full">
-          We’re sorry! This Form Is No Longer Here
+        <h1 class="text-dark text-2xl md:text-4xl leading-none font-medium text-center pt-4 md:pt-0 mx-auto md:max-w-full">
+          We're sorry! This Form Is No Longer Here
         </h1>
-        <p class="text-gray-600 mt-3">
-          It may have been deleted, deactivated, or the form URL might have been regenerated or updated by its owner. But don’t worry—you’ve got plenty of other options!
+        <p class="text-gray-500 mt-3 max-w-xl mx-auto">
+          It may have been deleted, deactivated, or the form URL might have been regenerated or updated by its owner. But don't worry—you've got plenty of other options!
         </p>
         
         <h3 class="my-8 text-dark text-xl font-semibold text-center md:pt-0 mx-auto md:max-w-full">
@@ -19,56 +19,20 @@
         </h3>
         <div class="mt-8 grid grid-cols-2 md:grid-cols-5 gap-4">
           <NuxtLink
-            :to="{ name: 'forms-create' }"
-            class="bg-blue-100/50 shadow-md rounded-lg p-4 text-center text-gray-700 hover:bg-blue-100 flex flex-col items-center"
+            v-for="(action, index) in actions"
+            :key="index"
+            :to="action.to"
+            :href="action.href"
+            class="bg-blue-100/50 rounded-lg px-4 py-6 text-center text-gray-700 hover:bg-blue-100 flex flex-col items-center hover:no-underline"
+            @click="action.onClick ? action.onClick() : null"
           >
             <Icon
-              name="heroicons:check-circle"
+              :name="action.icon"
               class="w-8 h-8 mb-2 text-nt-blue"
             />
-            Start Fresh
-          </NuxtLink>
-          <NuxtLink
-            :to="{ name: 'login' }"
-            class="bg-blue-100/50 shadow-md rounded-lg p-4 text-center text-gray-700 hover:bg-blue-100 flex flex-col items-center"
-          >
-            <Icon
-              name="heroicons:list-bullet"
-              class="w-8 h-8 mb-2 text-nt-blue"
-            />
-            Find Your Forms
-          </NuxtLink>
-          <NuxtLink
-            href="#"
-            class="bg-blue-100/50 shadow-md rounded-lg p-4 text-center text-gray-700 hover:bg-blue-100 flex flex-col items-center"
-            @click="crisp.openHelpdesk()"
-          >
-            <Icon
-              name="heroicons:question-mark-circle"
-              class="w-8 h-8 mb-2 text-nt-blue"
-            />
-            Need Help?
-          </NuxtLink>
-          <NuxtLink
-            :to="{ name: 'index' }"
-            class="bg-blue-100/50 shadow-md rounded-lg p-4 text-center text-gray-700 hover:bg-blue-100 flex flex-col items-center"
-          >
-            <Icon
-              name="heroicons:document-magnifying-glass"
-              class="w-8 h-8 mb-2 text-nt-blue"
-            />
-            Explore OpnForm
-          </NuxtLink>
-          <NuxtLink
-            href="#"
-            class="bg-blue-100/50 shadow-md rounded-lg p-4 text-center text-gray-700 hover:bg-blue-100 flex flex-col items-center"
-            @click="crisp.openChat()"
-          >
-            <Icon
-              name="heroicons:chat-bubble-left-right"
-              class="w-8 h-8 mb-2 text-nt-blue"
-            />
-            Need Assistance?
+            <span class="font-medium">
+            {{ action.text }}
+          </span>
           </NuxtLink>
         </div>
         
@@ -76,35 +40,70 @@
           <h3 class="mt-8 text-dark text-xl font-semibold text-center md:pt-0 mx-auto md:max-w-full">
             Still not sure what to do? Here are more ways to get started!
           </h3>
-          <p class="text-sm mt-4">
+          <p class="mt-4 text-gray-500">
             No worries! Here are some other options for you:
           </p>
           <ul class="mt-4 space-y-2 text-sm">
             <li>
               Do you want to know why users choose OpnForm? 
-              <NuxtLink :to="{ name: 'ai-form-builder' }">
+              <NuxtLink class="text-blue-500 hover:text-blue-600" :to="{ name: 'ai-form-builder' }">
                 Learn more here!
               </NuxtLink>
             </li>
             <li>
               Browse Templates
-              <NuxtLink :to="{ name: 'templates' }">
+              <NuxtLink class="text-blue-500 hover:text-blue-600" :to="{ name: 'templates' }">
                 Get inspired with ready-to-use form templates!
               </NuxtLink>
             </li>
             <li>
               Check Out Our Help Center
-              <NuxtLink to="https://help.opnform.com/en/">
+              <NuxtLink class="text-blue-500 hover:text-blue-600" to="https://help.opnform.com/en/">
                 Find quick answers to common questions.
               </NuxtLink>
             </li>
           </ul>
         </div>
       </div>
+
+      <div class="mt-20 border-t">
+        <OpenFormFooter />
+      </div>
+      
     </section>
   </main>
 </template>
 
 <script setup>
 const crisp = useCrisp()
+
+const actions = [
+  {
+    to: { name: 'forms-create-guest' },
+    icon: 'i-heroicons-check-circle',
+    text: 'Start Fresh'
+  },
+  {
+    to: { name: 'login' },
+    icon: 'i-heroicons-list-bullet',
+    text: 'Find Your Forms'
+  },
+  {
+    href: '#',
+    icon: 'i-heroicons-book-open',
+    text: 'Documentation',
+    onClick: () => crisp.openHelpdesk()
+  },
+  {
+    to: { name: 'index' },
+    icon: 'i-heroicons-document-magnifying-glass',
+    text: 'Explore OpnForm'
+  },
+  {
+    href: '#',
+    icon: 'i-heroicons-chat-bubble-left-right',
+    text: 'Need Assistance?',
+    onClick: () => crisp.openChat()
+  }
+]
 </script>
