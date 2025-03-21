a711b961d4fcfc90f6f7cc3627bacce5041bb9d9
Julien Nahum <julien@nahum.net>
2025-03-21 17:52:09 +0100

    Update label for image input in FormCustomization.vue from "Color image" to "Cover image" for improved clarity and consistency in form component labeling.

--- client/components/open/forms/components/form-components/FormCustomization.vue
+++ client/components/open/forms/components/form-components/FormCustomization.vue
@@ -145,7 +145,7 @@
     <image-input
       name="cover_picture"
       :form="form"
-      label="Color image"
+      label="Cover image"
       help="Not visible when form is embedded"
     />
     <toggle-switch-input
