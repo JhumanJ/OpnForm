fba2207e0ffb1fda97b13f1e5aaf9848f7e7669b
Chirag Chhatrala <60499540+chiragchhatrala@users.noreply.github.com>
2025-03-25 16:20:59 +0530

    Add 'exists_in_submissions' and 'does_not_exist_in_submissions' valid… (#725)
    * Add 'exists_in_submissions' and 'does_not_exist_in_submissions' validation conditions
    
    - Implement new validation conditions in FormLogicConditionChecker to check for existing submissions.
    - Update open_filters.json and client-side filters to include the new conditions.
    - Enhance FormLogicTest with test cases for the new validation conditions.
    - Modify UI components to support the new logic conditions in form validation.
    
    * Fix FormLogicConditionChecker
    
    * Improve custom_validation_only
    
    * fix test
    
    ---------
    
    Co-authored-by: Julien Nahum <julien@nahum.net>

--- client/components/open/forms/components/form-logic-components/ConditionEditor.client.vue
+++ client/components/open/forms/components/form-logic-components/ConditionEditor.client.vue
@@ -61,6 +61,7 @@ export default {
   props: {
     form: { type: Object, required: true },
     modelValue: { type: Object, required: false },
+    customValidation: { type: Boolean, default: false },
   },
   emits:  ['update:modelValue'],
 
@@ -79,12 +80,19 @@ export default {
         .map((property) => {
           const workspaceId = this.form.workspace_id
           const formSlug = this.form.slug
+          const customValidation = this.customValidation
           return {
             identifier: property.id,
             name: property.name,
             component: (function () {
               return defineComponent({
                 extends: ColumnCondition,
+                props: {
+                  customValidation: {
+                    type: Boolean,
+                    default: customValidation
+                  }
+                },
                 computed: {
                   property() {
                     return property
