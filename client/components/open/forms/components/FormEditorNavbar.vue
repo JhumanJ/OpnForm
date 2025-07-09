ae79acb852856690967125e23deae1c778d029bc
Julien Nahum <julien@nahum.net>
2025-07-09 19:57:51 +0200

    Refactor FormEditorNavbar to Enhance User Interaction and Consistency
    - Replaced the direct button implementation with the `UButton` component for improved styling and consistency.
    - Integrated the `TrackClick` component to track user interactions with the help button, enhancing analytics capabilities.
    - Updated button properties to utilize Tailwind CSS classes, improving responsiveness and visual appeal.
    - Simplified the button's label logic for better readability and maintainability.
    
    These changes aim to enhance user experience by providing a more consistent and interactive interface in the Form Editor Navbar.

--- client/components/open/forms/components/FormEditorNavbar.vue
+++ client/components/open/forms/components/FormEditorNavbar.vue
@@ -64,23 +64,20 @@
     <div
       class="flex items-stretch gap-x-2"
     >
-      <UTooltip
-        text="Help"
-        class="items-center relative"
-        :popper="{ placement: 'left' }"
-      >
-        <a
-          v-track.form_editor_help_button_clicked
-          href="#"
-          class="text-sm p-2 hover:bg-gray-100 cursor-pointer rounded-lg text-gray-500 hover:text-gray-800 cursor-pointer"
-          @click.prevent="crisp.openHelpdesk()"
+      <TrackClick name="form_editor_help_button_clicked">
+        <UTooltip
+          text="Help"
+          class="items-center relative"
+          :popper="{ placement: 'left' }"
         >
-          <Icon
-            name="heroicons:question-mark-circle"
-            class="w-5 h-5"
+          <UButton
+            color="ghost"
+            icon="i-heroicons-question-mark-circle"
+            class="p-2 text-gray-500 hover:text-gray-800"
+            @click.prevent="crisp.openHelpdesk()"
           />
-        </a>
-      </UTooltip>
+        </UTooltip>
+      </TrackClick>
       <slot name="before-save" />
       <UTooltip :popper="{ placement: 'left' }">
         <template #content>
@@ -101,29 +98,10 @@
             class="px-8 md:px-4 py-2"
             :loading="updateFormLoading"
             :class="saveButtonClass"
+            icon="i-ic-outline-save"
             @click="emit('save-form')"
-          >
-            <svg
-              class="w-4 h-4 text-white inline mr-1 -mt-1"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M17 21V13H7V21M7 3V8H15M19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H16L21 8V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21Z"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-            <template v-if="form.visibility === 'public'">
-              Publish Form
-            </template>
-            <template v-else>
-              Save Changes
-            </template>
-          </UButton>
+            :label="form.visibility === 'public' ? 'Publish Form' : 'Save Changes'"
+          />
         </TrackClick>
       </UTooltip>
     </div>
