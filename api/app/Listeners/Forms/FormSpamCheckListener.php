86534084312bcecae2b68a13e51fe01db50f1cbe
JhumanJ <julien@nahum.net>
2025-07-24 12:32:47 +0200

    Refactor FormSpamCheckListener to remove explicit queue dispatch
    - Updated the `handle` method in `FormSpamCheckListener.php` to dispatch the `CheckFormForSpam` job without specifying the queue, allowing it to use the default queue settings. This change simplifies the job dispatching process and ensures that the job is handled according to the default configuration.
    
    This change aims to streamline the spam check process by leveraging the default queue behavior, enhancing maintainability and reducing potential configuration issues.

--- api/app/Listeners/Forms/FormSpamCheckListener.php
+++ api/app/Listeners/Forms/FormSpamCheckListener.php
@@ -10,6 +10,6 @@ class FormSpamCheckListener
     public function handle(FormSaved $event): void
     {
         // We can add logic here to not queue for certain users if needed
-        CheckFormForSpam::dispatch($event->form)->onQueue('default');
+        CheckFormForSpam::dispatch($event->form);
     }
 }
