d9baa6d7368dec3bc590ea278c9d8ab554fd8577
Chirag Chhatrala <60499540+chiragchhatrala@users.noreply.github.com>
2025-10-31 21:26:07 +0530

    Better image input - unsplash (#939)
    * Add Unsplash integration for ImageInput
    
    - Introduced UnsplashController to fetch and cache images from the Unsplash API, providing a new endpoint for image retrieval.
    - Updated FontsController to use the Cache facade directly for improved readability.
    - Modified API routes to include the new Unsplash endpoint.
    - Enhanced ImageInput component to support image selection from Unsplash, adding a new tab for user interaction.
    - Created UnsplashImages component to display fetched images, allowing users to select images for upload.
    
    * Refactor Unsplash image fetching and enhance search functionality
    
    - Updated UnsplashController to handle search terms, allowing users to fetch images based on specific queries.
    - Implemented caching for curated images and improved error handling in the API response.
    - Enhanced UnsplashImages component with a search input, enabling users to search for images and clear the input easily.
    - Adjusted image selection logic to utilize the new response structure, ensuring proper data binding in the UI.
    
    * fix pint
    
    * Implement Unsplash download tracking and enhance image fetching
    
    - Added a new download method in UnsplashController to track image downloads via Unsplash API.
    - Updated the Unsplash API route to include the download endpoint.
    - Enhanced the UnsplashImages component to trigger download tracking when an image is selected.
    - Adjusted image fetching logic to limit results to 9 images and include additional photographer details in the response.
    - Improved the user interface to conditionally display Unsplash integration based on feature flags.
    
    * Refactor GoogleFontPicker component for improved loading state and font display
    
    - Simplified the loading state handling by introducing skeleton loaders for better user experience.
    - Enhanced the font fetching logic to ensure proper data binding and display.
    - Updated the template structure for clearer separation of loading and font display sections.
    
    ---------
    
    Co-authored-by: JhumanJ <julien@nahum.net>

--- api/routes/api.php
+++ api/routes/api.php
@@ -48,6 +48,9 @@ if (config('app.self_hosted')) {
 Route::group(['middleware' => 'auth.multi'], function () {
     Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
+    // Unsplash
+    Route::get('/unsplash', [\App\Http\Controllers\Content\UnsplashController::class, 'index'])->name('unsplash.index');
+    Route::post('/unsplash/download', [\App\Http\Controllers\Content\UnsplashController::class, 'download'])->name('unsplash.download');
 
     Route::get('user', [UserController::class, 'current'])->name('user.current');
     Route::delete('user', [UserController::class, 'deleteAccount']);
