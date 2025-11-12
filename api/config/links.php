cccc2ce1870d2790381ab88453d9830474844c16
JhumanJ <julien@nahum.net>
2025-11-12 12:13:19 +0100

    Update repository links in documentation and configuration files to reflect new GitHub organization
    - Changed all instances of the old GitHub repository URL from 'JhumanJ/OpnForm' to 'OpnForm/OpnForm' across README, API configuration, client configuration, and documentation files.
    - Ensured consistency in links for help, discussions, and cloning instructions to improve user experience and accessibility.

--- api/config/links.php
+++ api/config/links.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'help_url' => 'https://github.com/JhumanJ/OpnForm/discussions',
+    'help_url' => 'https://github.com/OpnForm/OpnForm/discussions',
     'helpdesk_sitemap_url' => 'https://notionforms.crisp.help/sitemap.xml',
     'changelog_url' => 'https://opnform.canny.io/changelog',
-    'github_url' => 'https://github.com/JhumanJ/OpnForm',
-    'github_forum_url' => 'https://github.com/JhumanJ/OpnForm/discussions',
+    'github_url' => 'https://github.com/OpnForm/OpnForm',
+    'github_forum_url' => 'https://github.com/OpnForm/OpnForm/discussions',
     'discord' => 'https://discord.gg/YTSjU2a9TS',
     'twitter' => 'https://twitter.com/OpnForm',
     'zapier_integration' => 'https://zapier.com/developer/public-invite/146950/58db583730cc46b821614468d94c35de/',
